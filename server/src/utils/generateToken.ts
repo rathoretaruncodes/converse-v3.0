e5e3219c355c11a413aed0721cb2e4497194f9aa
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-06-22 18:02:22 +0530

    fix: Resolve issue in generateToken.ts

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -9,7 +9,7 @@ const generateToken = (userId: string, res: Response) => {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,  //prevent XSS attacks
         sameSite: "strict",  //prevent CSRF attacks
-        secure: process.env.NODE_ENV ! == "development" //HTTPS
+        secure: process.env.NODE_ENV !== "development" //HTTPS
     });
     return token;
 };
